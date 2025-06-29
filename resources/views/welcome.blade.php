d1b1359e69d4a703b8568480bea8ab0c7349aa27
imranhasan22 <imran258hasan@gmail.com>
2025-06-29 16:21:37 +0600

    Image URL upadated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
                 </div>
             </div>
             <div class="col-lg-6">
-                <img src="/placeholder.svg?height=400&width=500" alt="Freelancer working" class="img-fluid rounded">
+                <img src="https://images.unsplash.com/photo-1637580681839-6e3ed197ca93?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Freelancer working" class="img-fluid rounded">
             </div>
         </div>
     </div>
